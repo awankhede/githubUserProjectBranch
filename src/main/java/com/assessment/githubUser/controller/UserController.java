438729cecb109974a72e0f3cebfe08fabfff4eab
awankhede <akumar92@gmail.com>
2025-12-12 11:30:58 -0600

    Create UserController.java

---
+++ src/main/java/com/assessment/githubUser/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.assessment.githubUser.controller;
+
+import com.assessment.githubUser.model.response.UserResponse;
+import com.assessment.githubUser.service.GithubUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private GithubUserService githubUserService;
+
+    @GetMapping("/users/{username}")
+    public ResponseEntity<UserResponse> getUerDetail(@PathVariable String username){
+        try {
+            UserResponse userResponse = githubUserService.getUserDetail(username);
+            return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        }  catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Runtime error", e);
+        }
+    }
+}
